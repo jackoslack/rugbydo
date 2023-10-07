95912c5486997d6e1b06787d6e885247f836102d
John Reynolds <john.reynolds@touchtechnology.com.au>
2023-10-08 10:39:01 +1100

    Add hello at home

--- rugbydo/urls.py
+++ rugbydo/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from .views import home_page_view
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home_page_view, name="home"),
 ]
