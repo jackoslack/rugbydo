9bc407880760d5f50331b76e695eec79d7a48cab
John Reynolds <john.reynolds@touchtechnology.com.au>
2023-10-14 07:37:52 +1100

    update

--- rugbydo/views.py
+++ rugbydo/views.py
@@ -26,7 +26,7 @@ def get_name(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect("/thanks/")
+            return HttpResponseRedirect("")
 
     # if a GET (or any other method) we'll create a blank form
     else:
