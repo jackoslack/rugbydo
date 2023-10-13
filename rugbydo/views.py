9dc4d167e2ea3e8c69ec7e72a41edcbe93871402
John Reynolds <john.reynolds@touchtechnology.com.au>
2023-10-14 07:44:24 +1100

    edit template

--- rugbydo/views.py
+++ rugbydo/views.py
@@ -26,7 +26,7 @@ def get_name(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect("")
+            return render(request, "name.html", {"name": name})
 
     # if a GET (or any other method) we'll create a blank form
     else:
