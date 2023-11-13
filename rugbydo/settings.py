4f0b089d753f537bae5632571dab9169a127a71f
John Reynolds <john.reynolds@touchtechnology.com.au>
2023-11-14 10:30:29 +1100

    -

--- rugbydo/settings.py
+++ rugbydo/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static') 
+STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, "staticfiles")]
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
