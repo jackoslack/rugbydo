7856be727b11497ecf178cfa7aea8e96a0e7c3c7
John Reynolds <john.reynolds@touchtechnology.com.au>
2023-11-14 11:52:06 +1100

    -

--- rugbydo/views.py
+++ rugbydo/views.py
@@ -16,10 +16,10 @@ def show_settings(request):
     project_root = settings.PROJECT_ROOT
 
     return render(request, 'bdir.html', 
-                  #{'static_url': static_url}, 
+                  {'static_url': static_url}, 
                   #{'static_files_directory': static_files_directory}, 
                   {'project_root': project_root},
-                  {'bdir': bdir},
+                  #{'bdir': bdir},
         )
 
 def home_page_view(request):
