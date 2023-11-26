06175e91ddca813993fa568c3090c3bb8e1c48b1
John Reynolds <john.reynolds@touchtechnology.com.au>
2023-11-27 06:32:10 +1100

    -

--- rugbydo/settings.py
+++ rugbydo/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'staticfiles')]
 
 STATIC_URL = 'static/'
 
+STATIC_URL = 'rugbydo/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
