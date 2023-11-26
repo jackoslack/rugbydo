6a7767b97d062f925c72c2b8401516bf50b06cb2
John Reynolds <john.reynolds@touchtechnology.com.au>
2023-11-26 15:47:04 +1100

    -

--- rugbydo/urls.py
+++ rugbydo/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     path("", home_page_view, name="home"),
     path("bdir", show_settings, name="bdir"),
     path("name", get_name, name="name"),
-    path("players/{gender: "None"}", players, name="players"),
+    path("players/{gender: 'None'}", players, name="players"),
     path("mlist", mlist, name="mlist"),
     
     
